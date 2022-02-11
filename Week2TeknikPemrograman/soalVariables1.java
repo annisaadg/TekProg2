0e90cfa026fd9128429df81e9236f26c1246f5b1
Annisa DG <94848415+asiandolce@users.noreply.github.com>
2022-02-12 01:11:22 +0700

    Create soalVariables1.java
    Implementation the usage of "final double"

---
+++ Week2TeknikPemrograman/soalVariables1.java
@@ -0,0 +1,20 @@
+package soalVariables;
+/**
+ * @author Annisa Dinda
+ * @version 1.0
+ * @since 2022-02-11
+ */
+
+public class soalVariables {
+    /**
+     * @param args 
+     */
+    public static void main(String[] args) {
+        final double CM_PER_INCH = 2.54;
+        double paperWidth = 8.5;
+        double paperHeight = 11;
+        System.out.println("Paper size in centimeters: " +
+        paperWidth * CM_PER_INCH + " by " + paperHeight *
+        CM_PER_INCH);
+    }
+}
