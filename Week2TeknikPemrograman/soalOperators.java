294bbc78b5d6d4746f777a4d92ebedc22614d3b2
Annisa DG <94848415+asiandolce@users.noreply.github.com>
2022-02-12 01:23:25 +0700

    Create soalOperators.java
    The soalOperators program converts a double (float) data type into an integer by using math.rounding

---
+++ Week2TeknikPemrograman/soalOperators.java
@@ -0,0 +1,20 @@
+package soalOperators;
+
+/**
+ * The soalOperators program converts a double (float) data
+ * type into an integer by using math.rounding
+ * @author Annisa Dinda
+ * @version 1.0
+ * @since 2022-02-11
+ */
+public class soalOperators {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double x = 92.98;
+        int nx = (int) Math.round(x);
+    }
+}
