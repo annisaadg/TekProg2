e842ea9c9005f27bf4a0ac49f262d2874bb6ca2d
Annisa DG <94848415+asiandolce@users.noreply.github.com>
2022-02-26 19:46:55 +0800

    Create KelasDua.java

---
+++ Week4TeknikPemrograman/StudyCase3/KelasDua.java
@@ -0,0 +1,21 @@
+package SC3;
+
+/**
+ *
+ * @author Annisa
+ * @since 24/02/2022
+ * @version 1.0
+ */
+class KelasDua
+{
+    {
+        System.out.println(5);
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(6);
+        KelasSatu satu = new KelasSatu();
+        KelasSatu dua = new KelasSatu(10);
+    }
+}
