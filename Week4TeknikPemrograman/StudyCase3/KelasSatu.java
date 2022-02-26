8cffb8d0b632758d1d15dcb4e23fa816ed6c9e94
Annisa DG <94848415+asiandolce@users.noreply.github.com>
2022-02-26 19:46:28 +0800

    Create KelasSatu.java

---
+++ Week4TeknikPemrograman/StudyCase3/KelasSatu.java
@@ -0,0 +1,26 @@
+package SC3;
+
+/**
+ *
+ * @author Annisa 
+ * @since 24/02/2022
+ * @version 1.0
+ */
+class KelasSatu
+{
+    {
+        System.out.println(11);
+    }
+    static
+    {
+        System.out.println(2);
+    }
+        public KelasSatu(int i)
+    {
+        System.out.println(3);
+    }
+        public KelasSatu()
+    {
+        System.out.println(4);
+    }
+}
