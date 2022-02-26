c27422e61ac22517d0add54ffe8136eafe0bd644
Annisa DG <94848415+asiandolce@users.noreply.github.com>
2022-02-26 19:44:21 +0800

    Create UpinIpin.java

---
+++ Week4TeknikPemrograman/StudyCase2/UpinIpin.java
@@ -0,0 +1,18 @@
+package SC2;
+
+/**
+ *
+ * @author Annisa 
+ * @since 24/02/2022
+ * @version 1.0
+ */
+public class UpinIpin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Item name = new Item("upin");
+    }
+    
+}
