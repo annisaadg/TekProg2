38d5feb521dfe9eadb73f7649e01f355274adce4
Annisa DG <94848415+asiandolce@users.noreply.github.com>
2022-03-13 23:25:12 +0700

    Create NarutoAnime.java

---
+++ Week5TeknikPemrograman/Kasus3/NarutoAnime.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author Annisa
+ */
+public class NarutoAnime {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Sasuke s = new Sasuke();
+        s.printDojutsu();
+        Itachi i = new Sasuke();
+        i.printKekkeiGenkai();
+    }
+}
