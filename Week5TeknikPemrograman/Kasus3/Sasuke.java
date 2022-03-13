6dbe6a62250b96e3d5e03d0954069e4d252884a0
Annisa DG <94848415+asiandolce@users.noreply.github.com>
2022-03-13 23:24:30 +0700

    Create Sasuke.java

---
+++ Week5TeknikPemrograman/Kasus3/Sasuke.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author Annisa
+ */
+public class Sasuke extends Itachi{
+    String Dojutsu = "Sharingan";
+    
+    void printDojutsu(){
+        super.printDojutsu();
+        System.out.println(this.Dojutsu);
+    }
+}
