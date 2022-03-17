2fbe5e2c14f9e96bd340ac1b72c039e7741fbbd9
annisaadg <100127500+annisaadg@users.noreply.github.com>
2022-03-17 23:59:31 +0700

    Create University.java

---
+++ Week6TeknikPemrograman/Relationship/association/University.java
@@ -0,0 +1,19 @@
+/**
+*
+* @author Annisa
+* @since 17/03/2022
+*/
+
+package relationship.association;
+
+class University {
+    private String name;
+
+    University(String name) {
+        this.name = name;
+    }
+
+    public String getUniversityName() {
+        return this.name;
+    }
+}
