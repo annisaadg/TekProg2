067fded96486d4f6b45dbe742f2d16a767a95a4e
annisaadg <100127500+annisaadg@users.noreply.github.com>
2022-03-17 22:39:23 +0700

    Create LineItem.java

---
+++ Week6TeknikPemrograman/Relationship/composition/LineItem.java
@@ -0,0 +1,50 @@
+/**
+*
+* @author Annisa
+* @since 17/03/2022
+*/
+
+package relationship.composition;
+
+public class LineItem {
+    private int id;
+    private int quantity;
+    private Product p;
+
+    public LineItem(int id, int quantity, Product p) {
+        super();
+        this.id = id;
+        this.quantity = quantity;
+        this.p = p;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+    public Product getP() {
+        return p;
+    }
+
+    public void setP(Product p) {
+        this.p = p;
+    }
+
+    @Override
+    public String toString() {
+        return "LineItem id= " + id + "\n     \t  >>LineItem quantity= " + quantity + "\n     \t    LineItem Product: " + p + "";
+    }
+
+}
