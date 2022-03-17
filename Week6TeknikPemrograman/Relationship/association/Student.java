f5d082798e3ba84bf3091eb881bf3d4f0fe8a7a8
annisaadg <100127500+annisaadg@users.noreply.github.com>
2022-03-17 23:59:59 +0700

    Create Student.java

---
+++ Week6TeknikPemrograman/Relationship/association/Student.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @author Annisa
+ * @since 17/03/2022
+ */
+
+package relationship.association;
+
+public class Student {
+    private String name;
+
+    Student(String name) {
+        this.name = name;
+    }
+
+    public String getStudentName() {
+        return this.name;
+    }
+}
