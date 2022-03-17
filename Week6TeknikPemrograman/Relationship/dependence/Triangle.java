6f6ec884acee9f2f6d921c774d26ca7b2f0b5f76
annisaadg <100127500+annisaadg@users.noreply.github.com>
2022-03-17 22:46:43 +0700

    Create Triangle.java

---
+++ Week6TeknikPemrograman/Relationship/dependence/Triangle.java
@@ -0,0 +1,14 @@
+/**
+*
+* @author Annisa
+* @since 17/03/2022
+*/
+
+package relationship.dependence;
+
+public class Triangle implements Shape{
+     @Override
+     public void drawShape() {
+         System.out.println("Triangle is drawn.");
+     }  
+}
