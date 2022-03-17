d0204a632acae9902ac7a2ee4b84db93fa15c29e
annisaadg <100127500+annisaadg@users.noreply.github.com>
2022-03-17 22:41:48 +0700

    Create Product.java

---
+++ Week6TeknikPemrograman/Relationship/composition/Product.java
@@ -0,0 +1,49 @@
+/**
+*
+* @author Annisa
+* @since 17/03/2022
+*/
+
+package relationship.composition;
+
+public class Product {
+    private int id;
+    private String name;
+    private String description;
+
+    public Product(int id, String name, String description) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return " Product [id= " + id + ", name= " + id + ", description= " + description + "]";
+    }
+}
