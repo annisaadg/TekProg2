036e06a10d9035c381c19a6e3dac85a22278c882
annisaadg <100127500+annisaadg@users.noreply.github.com>
2022-03-17 22:45:30 +0700

    Create Rectangle.java

---
+++ Week6TeknikPemrograman/Relationship/dependence/Rectangle.java
@@ -0,0 +1,15 @@
+/**
+*
+* @author Annisa
+* @since 17/03/2022
+*/
+
+package relationship.dependence;
+
+public class Rectangle implements Shape {
+    
+    @Override
+    public void drawShape() {
+        System.out.println("Rectangle is drawn.");
+    }
+}
