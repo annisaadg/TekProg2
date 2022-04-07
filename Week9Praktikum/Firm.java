30b5a553c96476db28dca58a4c5e2e1e7c65db60
annisaadg <100127500+annisaadg@users.noreply.github.com>
2022-04-07 11:21:51 +0700

    Delete Week9Praktikum directory

--- Week9Praktikum/Firm.java
+++
@@ -1,9 +0,0 @@
-package case73;
-
-public class Firm {
-    //Creates a staff of employee for a firm and pays them
-    public static void main(String[] args) {
-        Staff personnel = new Staff();
-        personnel.payday();
-    }
-}
